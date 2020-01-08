32a52012eaec11954e5f641bd429fca8f7158acf
Gabriel Răileanu <raileanu.gabriel97@gmail.com>
2020-01-09 00:17:12 +0200

    Added value for node when it has no parents.

--- BayesianNetworkMedicalApplication/DataModel/NodeGeneric.cs
+++ BayesianNetworkMedicalApplication/DataModel/NodeGeneric.cs
@@ -18,21 +18,30 @@ namespace DataModel
     {
         private const int MAX_PARENTS = 10;
 
-        //
-        //public int NoOfParents { get; set; }
-
         public List<NodeGeneric> ListOfParents;
-        //nume nod
         public string Name { get; set; }
         public List<TextBox> ProbTrue;
         public List<TextBox> ProbFalse;
         //valoare observata bool sau index
+
+            // NO LONGER USED
         public List<NodeGeneric> ListOfChildren;
         public IsUsed NodeStatus;
         public bool IsObservable { get; set; }
 
         // TODO: decide when this needs to be populated & allocate memory for it.
+        // Also, this can replace the list for ProbTrue/ProbFalse
+        // Should be allocated with 2*nX 2 linesXcolumns, where n = noParents.
+        /// <summary>
+        /// The structure is:
+        /// FIRST PARENT    SECOND PARENT   NODE YES    NODE NO
+        /// YES             YES             value       value
+        /// YES             NO             value       value
+        /// NO             YES             value       value
+        /// NO             NO             value       value
+        /// </summary>
         public double[,] probabilities=null;
+
         public NodeGeneric()
         {
             ListOfParents = new List<NodeGeneric>();
@@ -56,6 +65,21 @@ namespace DataModel
 
         public static double ComputeBayes(NodeGeneric node)
         {
+            // The probability of a node is:
+            // the probability of that node if has no parents
+            // the probability conditionated by the parents if it has any
+            if (node.ListOfParents.Count == 0)
+            {
+                if (node.NodeStatus == IsUsed.TRUE)
+                {
+                    return node.probabilities[0, 0];
+                } else if (node.NodeStatus == IsUsed.FALSE)
+                {
+                    return node.probabilities[0, 1];
+                }
+           }
+
+
             if (node.ListOfParents.Count==0)
             {
                 return double.Parse(node.ProbTrue.FirstOrDefault().Text);
