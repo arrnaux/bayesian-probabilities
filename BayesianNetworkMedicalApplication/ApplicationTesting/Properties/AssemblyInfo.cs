1664e8579ce8a8ebc21c1af3b117c62c28a9bcea
NicuBoca <mail.boca.nicolae@gmail.com>
2020-04-26 15:22:51 +0300

    Updated Help & SRS and added ApplicationTesting project

---
+++ BayesianNetworkMedicalApplication/ApplicationTesting/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ApplicationTesting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ApplicationTesting")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("870dc183-5c9a-4ca1-bd30-7e1737c6ff9e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
