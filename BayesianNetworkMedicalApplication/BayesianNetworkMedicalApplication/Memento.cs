334c7466f8aab3a3d74a50b107868e3bed346627
ignatescu_s <ignatescustefan@gmail.com>
2020-05-24 19:26:34 +0300

    added hedears

--- BayesianNetworkMedicalApplication/BayesianNetworkMedicalApplication/Memento.cs
+++ BayesianNetworkMedicalApplication/BayesianNetworkMedicalApplication/Memento.cs
@@ -1,4 +1,20 @@
-﻿using System.Collections.Generic;
+﻿/**************************************************************************
+ *                                                                        *
+ *  File:        Memento.cs                                               *
+ *  Copyright:   (c) 2020 Gabriel Răileanu, Nicolae Boca, Ștefan Ignătescu*
+ *  Website:     https://github.com/arrnaux/bayesian-probabilities        *
+ *  Description: Generates file information headers.                      *
+ *                                                                        *
+ *  This program is free software; you can redistribute it and/or modify  *
+ *  it under the terms of the GNU General Public License as published by  *
+ *  the Free Software Foundation. This program is distributed in the      *
+ *  hope that it will be useful, but WITHOUT ANY WARRANTY; without even   *
+ *  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR   *
+ *  PURPOSE. See the GNU General Public License for more details.         *
+ *                                                                        *
+ **************************************************************************/
+
+using System.Collections.Generic;
 
 namespace BayesianNetworkInterface
 {
